97f7c18a55cc830e22f23b0c89e65c272a06ed0c
Goshitha Premachandra <wkgoshithapremachandra@gmail.com>
2024-09-09 12:19:01 +0530

    implement footer components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+    <div>
+        <footer className="bg-gray-800 p-4 text-white text-center">
+            <p>&copy; 2021 All rights reserved</p>
+        </footer>
+    </div>
+    </>
+  )
+}
