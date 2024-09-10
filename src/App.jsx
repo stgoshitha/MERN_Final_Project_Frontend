154ba37b632193124cf3f50c7431e4ce0373e431
Goshitha Premachandra <wkgoshithapremachandra@gmail.com>
2024-09-10 22:23:10 +0530

    route the account.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,43 @@
-import React from 'react'
-import { BrowserRouter , Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import AdminDashboad from './pages/AdminDashboad'
-import { Toaster } from 'react-hot-toast'
-import AdminLayouts from './Layouts/AdminLayouts'
-import UserLayouts from './Layouts/UserLayouts'
-import PublicLayouts from './Layouts/PublicLayouts'
-import AdminManage from './pages/AdminManage'
-import ViewHome from './pages/ViewHome'
-
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import AdminDashboad from './pages/AdminDashboad';
+import { Toaster } from 'react-hot-toast';
+import AdminLayouts from './Layouts/AdminLayouts';
+import UserLayouts from './Layouts/UserLayouts';
+import PublicLayouts from './Layouts/PublicLayouts';
+import AdminManage from './pages/AdminManage';
+import ViewHome from './pages/ViewHome';
+import Account from './pages/Account';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <Toaster />
+        <Toaster />
         <Routes>
-          <Route path='/admin' element={<AdminLayouts/>}>
-          <Route index element={<Home/>}/>
-          <Route path='admindash' element={<AdminDashboad />} />
-          <Route path='adminManage' element={<AdminManage />} />
-          
-          </Route>
-          
-          <Route path='/' element={<UserLayouts/>}>
-          <Route index element={<Home/>}/>
+          <Route path='/admin' element={<AdminLayouts />}>
+            <Route index element={<Home />} />
+            <Route path='admindash' element={<AdminDashboad />} />
+            <Route path='adminManage' element={<AdminManage />} />
           </Route>
 
-          <Route path='/' element={<PublicLayouts/>}>
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="home" element={<ViewHome />} />
+          <Route path='/' element={<UserLayouts />}>
+            <Route index element={<Home />} />
+            <Route path='account' element={<Account />} />
           </Route>
-          
+
+          <Route path='/' element={<PublicLayouts />}>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="home" element={<ViewHome />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-
-export default App
+export default App;
